cc7094877d66a620f2b89abe8361bf9b00b5e549
Waffen Sultan <waffen.sultanofc@gmail.com>
2024-03-23 21:06:02 +0800

    Changes to code

---
+++ README.md
@@ -0,0 +1,1 @@
+A basic CLI app that gives file size information of a given file.
--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,31 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"log"
 	"os"
 )
 
 func main() {
-	// Check for flags
-	if len(os.Args) <= 1 {
-		log.Fatal("\nUsage: go <program_name> <file_name>")
+	defer os.Exit(0)
+
+	flags := os.Args
+
+	if len(flags) <= 2 {
+		log.Fatal("\nUsage: go <program_name> <command: --c, --l> <file_name>")
 	}
-	// Check for help command
-	for _, flag := range os.Args {
-		if flag == "--help" {
-			log.Fatal("\nHelp command currently does not exist")
+
+	file, err := os.Open(flags[2])
+	if err != nil {
+		log.Fatal("\nFile does not exist")
+	}
+
+	for _, flag := range flags {
+		switch flag {
+		case "--help":
+			log.Fatal("\nHelp command currently not implemented")
+		case "--c":
+			getFileSize(file)
+		case "--l":
+			getFileTotalLines(file)
 		}
 	}
 
-	file, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatal("\nFile: ", os.Args[1], " does not exist")
-	}
-	info, err := file.Stat()
+	file.Close()
+}
+
+func getFileSize(f *os.File) {
+	info, err := f.Stat()
 	if err != nil {
 		log.Fatal("\nCould not obtain file information")
 	}
-	log.Println("\nFile:", os.Args[1], "\nSize:", info.Size())
 
-	defer os.Exit(0)
+	log.Println("\nFile:", os.Args[2], "\nSize:", info.Size())
+}
+
+func getFileTotalLines(f *os.File) {
+	scanner := bufio.NewScanner(f)
+	line := 0
+	for scanner.Scan() {
+		line++
+	}
+
+	log.Println("Total lines:", line)
 }
